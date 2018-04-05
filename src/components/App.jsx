f9a9fad49556f1444df3271231027d8d9c67057e
d-weeteling <dirk.weeteling@nelen-schuurmans.nl>
2018-04-05 10:16:56 +0200

    rm'd traces of lizard-api-client inclusion (no current need for SSO usage)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,63 +2,65 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import ReactDOM from "react-dom";
 
-import { userIsLoggedIn } from "../session.js";
+// 05-04-2018: See comment in imported file
+// import { userIsLoggedIn } from "../session.js";
 import { AppPrivate } from "./AppPrivate.jsx";
 
 class Main extends Component {
-  constructor () {
-    super();
-    this.state = {
-      isfetchingBootstrapData: true,
-      isLoggedIn: null,
-      bootstrapData: null,
-      login: null,
-      logout: null,
-      error: null
-    };
-  }
-  componentWillMount() {
-    userIsLoggedIn().then(
-      bootstrap => {
-        if (bootstrap.authenticated) {
-          this.setState({
-            isfetchingBootstrapData: false,
-            isLoggedIn: true,
-            bootstrapData: bootstrap,
-            logout: bootstrap.doLogout.bind(bootstrap)
-          });
-        } else {
-          this.setState({
-            isfetchingBootstrapData: false,
-            isLoggedIn: false,
-            bootstrapData: bootstrap,
-            login: bootstrap.doLogin.bind(bootstrap)
-          });
-        }
-      },
-      error => {
-        this.setState({
-          isfetchingBootstrapData: false,
-          isLoggedIn: false,
-          bootstrapData: bootstrap,
-          error: error
-        });
-      }
-    );
-  }
+  // constructor () {
+  //   super();
+  //   this.state = {
+  //     isfetchingBootstrapData: true,
+  //     isLoggedIn: null,
+  //     bootstrapData: null,
+  //     login: null,
+  //     logout: null,
+  //     error: null
+  //   };
+  // }
+  //componentWillMount() {
+    // userIsLoggedIn().then(
+    //   bootstrap => {
+    //     if (bootstrap.authenticated) {
+    //       this.setState({
+    //         isfetchingBootstrapData: false,
+    //         isLoggedIn: true,
+    //         bootstrapData: bootstrap,
+    //         logout: bootstrap.doLogout.bind(bootstrap)
+    //       });
+    //     } else {
+    //       this.setState({
+    //         isfetchingBootstrapData: false,
+    //         isLoggedIn: false,
+    //         bootstrapData: bootstrap,
+    //         login: bootstrap.doLogin.bind(bootstrap)
+    //       });
+    //     }
+    //   },
+    //   error => {
+    //     this.setState({
+    //       isfetchingBootstrapData: false,
+    //       isLoggedIn: false,
+    //       bootstrapData: bootstrap,
+    //       error: error
+    //     });
+    //   }
+    // );
+  //}
   render () {
-    if (this.state.isfetchingBootstrapData) {
-      return null;
-    } else {
-      if (this.state.error) {
-        return <div>There was an error</div>;
-      } else if (this.state.isLoggedIn) {
-        return <AppPrivate firstName={this.state.bootstrapData.first_name} />
-      } else {
-        // TODO: auto-redirect to SSO when not logged in......
-        return (<div>Your will be redirected to the SSO soon...</div>);
-      }
-    }
+    return <AppPrivate firstName="Anonymous" />;
+    // if (this.state.isfetchingBootstrapData) {
+    //   return null;
+    // } else {
+    //   if (this.state.error) {
+    //     return <div>There was an error</div>;
+    //   } else if (this.state.isLoggedIn) {
+    //     return <AppPrivate firstName={this.state.bootstrapData.first_name} />
+    //   } else {
+    //     // TODO: auto-redirect to SSO when not logged in......
+    //     return (<div>Your will be redirected to the SSO soon...</div>);
+    //   }
+    // }
   }
 }
 
--- src/session.js
+++ src/session.js
@@ -1,5 +1,10 @@
-import { getBootstrap } from "lizard-api-client";
+// Comment 05-04-2018: We don't need/want any lizard-api-client shennanigans in
+// our first crop-growth  off.release/deployment, because 1) its not needed for
+// Bangladesh anyway and 2) we'll do it (properly) later on.
+///////////////////////////////////////////////////////////////////////////////
 
-export function userIsLoggedIn() {
-  return getBootstrap();
-}
+// import { getBootstrap } from "lizard-api-client";
+
+// export function userIsLoggedIn() {
+//   return getBootstrap();
+// }
