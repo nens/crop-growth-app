755a62a2be9e6a533ca6f6c2bac127e8700c71b2
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2019-02-11 17:22:02 +0100

    change uuid

--- src/constants.js
+++ src/constants.js
@@ -8,7 +8,7 @@ export const COUNTRY = "BANGLADESH";
 
 export const countryConfig = {
   BANGLADESH: {
-    raster: "62bb4a86-2a13-414c-a111-486c1dd9b0af",
+    raster: "9f46fbb8-5b4b-46dc-97f6-06c515a858c1",
     pixelSize: 0.002,
     uri:
       "/api/v3/raster-aggregates/?agg=counts&rasters=62bb4a8&srs=EPSG:4326&styles=GrowthStage_Rice_D",
